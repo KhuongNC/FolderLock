e632e8854281655b3943c275edfa4dbbf47b23a3
Nguyen Cong Khuong <anhkhuongmurom@gmail.com>
2022-04-28 23:18:08 +0700

    Create functions for Main form

--- FoldeLock/FrmSetPass.Designer.cs
+++ FoldeLock/FrmSetPass.Designer.cs
@@ -29,12 +29,147 @@ namespace FoldeLock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.ButtonSet = new System.Windows.Forms.Button();
+            this.CheckBoxShowPassword = new System.Windows.Forms.CheckBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(54, 9);
+            this.Label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(351, 25);
+            this.Label3.TabIndex = 18;
+            this.Label3.Text = "Set Your User Name and Password";
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = global::FoldeLock.Properties.Resources.Login_Manager;
+            this.PictureBox1.Location = new System.Drawing.Point(13, 38);
+            this.PictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(168, 148);
+            this.PictureBox1.TabIndex = 19;
+            this.PictureBox1.TabStop = false;
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.Location = new System.Drawing.Point(358, 130);
+            this.ButtonClose.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(68, 28);
+            this.ButtonClose.TabIndex = 26;
+            this.ButtonClose.Text = "Close";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // ButtonSet
+            // 
+            this.ButtonSet.Location = new System.Drawing.Point(287, 130);
+            this.ButtonSet.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonSet.Name = "ButtonSet";
+            this.ButtonSet.Size = new System.Drawing.Size(68, 28);
+            this.ButtonSet.TabIndex = 25;
+            this.ButtonSet.Text = "Set";
+            this.ButtonSet.UseVisualStyleBackColor = true;
+            this.ButtonSet.Click += new System.EventHandler(this.ButtonSet_Click);
+            // 
+            // CheckBoxShowPassword
+            // 
+            this.CheckBoxShowPassword.AutoSize = true;
+            this.CheckBoxShowPassword.Location = new System.Drawing.Point(287, 105);
+            this.CheckBoxShowPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.CheckBoxShowPassword.Name = "CheckBoxShowPassword";
+            this.CheckBoxShowPassword.Size = new System.Drawing.Size(129, 21);
+            this.CheckBoxShowPassword.TabIndex = 24;
+            this.CheckBoxShowPassword.Text = "Show Password";
+            this.CheckBoxShowPassword.UseVisualStyleBackColor = true;
+            this.CheckBoxShowPassword.CheckedChanged += new System.EventHandler(this.CheckBoxShowPassword_CheckedChanged);
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(198, 73);
+            this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(75, 18);
+            this.Label2.TabIndex = 23;
+            this.Label2.Text = "Password";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(191, 39);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(84, 18);
+            this.Label1.TabIndex = 22;
+            this.Label1.Text = "User Name";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(287, 72);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(137, 22);
+            this.txtPass.TabIndex = 21;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(287, 38);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(137, 22);
+            this.txtUser.TabIndex = 20;
+            // 
+            // FrmSetPass
+            // 
+            this.AcceptButton = this.ButtonSet;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(458, 195);
+            this.Controls.Add(this.ButtonClose);
+            this.Controls.Add(this.ButtonSet);
+            this.Controls.Add(this.CheckBoxShowPassword);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.PictureBox1);
+            this.Controls.Add(this.Label3);
+            this.Name = "FrmSetPass";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FrmSetPass";
+            this.Load += new System.EventHandler(this.FrmSetPass_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.PictureBox PictureBox1;
+        internal System.Windows.Forms.Button ButtonClose;
+        internal System.Windows.Forms.Button ButtonSet;
+        internal System.Windows.Forms.CheckBox CheckBoxShowPassword;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtPass;
+        internal System.Windows.Forms.TextBox txtUser;
     }
 }
--- FoldeLock/FrmSetPass.cs
+++ FoldeLock/FrmSetPass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoldeLock.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,49 @@ namespace FoldeLock
         public FrmSetPass()
         {
             InitializeComponent();
+        }
+
+        private void FrmSetPass_Load(object sender, EventArgs e)
+        {
+            txtPass.UseSystemPasswordChar = true;
+        }
+
+        private void ButtonSet_Click(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "")
+            {
+                MessageBox.Show("Please Enter your user name");
+                txtUser.Focus();
+            }
+            else if (txtPass.Text == "")
+            {
+                MessageBox.Show("Please Enter your password");
+                txtPass.Focus();
+            }
+            else
+            {
+                Settings.Default.user = txtUser.Text.Trim();
+                Settings.Default.password = txtPass.Text.Trim();
+                Settings.Default.Save();
+                this.Close();
+            }
+        }
+
+        private void ButtonClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CheckBoxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBoxShowPassword.CheckState == CheckState.Checked)
+            {
+                txtPass.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                txtPass.UseSystemPasswordChar = true;
+            }
         }
     }
 }
