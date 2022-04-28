f1a2b5c1a4ac8bc075999011e8540018567d6bc0
Nguyen Cong Khuong <anhkhuongmurom@gmail.com>
2022-04-28 22:23:44 +0700

    Update location of Resource folder. Create Log in UI

--- FoldeLock/frmLogin.Designer.cs
+++ FoldeLock/frmLogin.Designer.cs
@@ -29,13 +29,132 @@ namespace FoldeLock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.ButtonOk = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label3.Location = new System.Drawing.Point(295, 25);
+            this.Label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(95, 35);
+            this.Label3.TabIndex = 23;
+            this.Label3.Text = "LogIn";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label2.Location = new System.Drawing.Point(197, 98);
+            this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(75, 18);
+            this.Label2.TabIndex = 22;
+            this.Label2.Text = "Password";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(190, 66);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(84, 18);
+            this.Label1.TabIndex = 21;
+            this.Label1.Text = "User Name";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(286, 96);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(137, 22);
+            this.txtPass.TabIndex = 20;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(286, 64);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(4);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(137, 22);
+            this.txtUser.TabIndex = 19;
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.BackgroundImage = global::FoldeLock.Properties.Resources.button_cancel;
+            this.ButtonCancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonCancel.Location = new System.Drawing.Point(368, 126);
+            this.ButtonCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(55, 47);
+            this.ButtonCancel.TabIndex = 25;
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            // 
+            // ButtonOk
+            // 
+            this.ButtonOk.BackgroundImage = global::FoldeLock.Properties.Resources.button_ok;
+            this.ButtonOk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ButtonOk.Location = new System.Drawing.Point(284, 126);
+            this.ButtonOk.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonOk.Name = "ButtonOk";
+            this.ButtonOk.Size = new System.Drawing.Size(55, 47);
+            this.ButtonOk.TabIndex = 24;
+            this.ButtonOk.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FoldeLock.Properties.Resources.Login_Manager;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 25);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 145);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClientSize = new System.Drawing.Size(451, 178);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.ButtonOk);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox txtPass;
+        internal System.Windows.Forms.TextBox txtUser;
+        internal System.Windows.Forms.Button ButtonCancel;
+        internal System.Windows.Forms.Button ButtonOk;
     }
 }
 
